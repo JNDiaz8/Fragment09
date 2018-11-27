6c15213d291e4993633e45768ad2e6b6595a2dde
Joaquin Diaz Ramirez <joaquin.diaz.ramirez.8897@gmail.com>
2018-11-27 14:16:26 +0100

    Subida del Proyecto Fragment09. Tarea de acabar en casa.

---
+++ app/src/main/java/matinal/example/com/fragment09/Fragment09.java
@@ -0,0 +1,13 @@
+package matinal.example.com.fragment09;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Fragment09 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment09);
+    }
+}
